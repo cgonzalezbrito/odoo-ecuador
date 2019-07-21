7efce25cb5573b0f70fd79c34fc7a05929fcc90b
cgonzalezbrito <cgonzalezbrt@gmail.com>
2019-07-20 23:10:55 -0500

    [ADD] credit in POS payment screen

--- l10n_ec_pos_refund/static/src/js/pos_refund.js
+++ l10n_ec_pos_refund/static/src/js/pos_refund.js
@@ -1,19 +1,18 @@
 odoo.define('l10n_ec_pos_refund', function(require){
 "use strict";
 
+    var PosDB = require('point_of_sale.DB');
     var models = require('point_of_sale.models');
-    
-    var _super_pos = models.PosModel.prototype;
-    models.PosModel = models.PosModel.extend({
-    //local.WidgetM = instance.web.form.AbstractField.extend({
-        initialize: function(session, attributes){
-            _super_pos.initialize.call(this, session, attributes);
-            //this.$el.html(QWeb.render("WidgetTemplate"));
-            this.field_manager.on("load_record", this, function() {
-                models.load_fields('res.partner', ['refund_credit']);
-                this._trigger();
-            });
-            return this._super();  
+    var screens = require('point_of_sale.screens');
+
+    screens.PaymentScreenWidget.include({
+        
+        customer_changed: function() {
+            var client = this.pos.get_client();
+            this.$('.js_customer_name').text( client ? client.name : _t('Customer') ); 
+            console.log(client.refund_credit);
+            this.$('.js_customer_credit').text(client.refund_credit)
         },
     });
+
 });
