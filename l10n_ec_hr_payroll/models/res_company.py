9f9c6f3a52c817952dbd480861b18384823334ec
cgonzalezbrito <cgonzalezbrt@gmail.com>
2019-10-16 22:31:35 -0500

    [IMP] payroll frontend

--- l10n_ec_hr_payroll/models/res_company.py
+++ l10n_ec_hr_payroll/models/res_company.py
@@ -6,12 +6,12 @@ from odoo import _, api, fields, models
 class res_company(models.Model):
     _inherit = 'res.company'
 
-    basic_wage = fields.Float(_('Basic Wage ($)'), oldname='sueldo_basico')
+    basic_wage = fields.Float(_('Salario Básico ($)'), oldname='sueldo_basico')
     iess_representante_legal = fields.Float('Contribución Representante legal IESS (%)', )
     iess_personal = fields.Float('Contribución personal IESS (%)')
     iess_empleador = fields.Float('Contribución patronal IESS (%)')
     porcentaje_fondos_reserva = fields.Float('Fondos de reserva (%)')  # TODO calcular
     default_payroll_journal_id = fields.Many2one(
-        'account.journal', string="Default Payroll Journal",
+        'account.journal', string="Diario por defecto",
         domain="[('type','=','general')]",
         help="This journal will be used on payroll creation", )
