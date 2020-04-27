a9e32084077bbe2cc7e006b1524809a6c27ffcf7
cgonzalezbrito <cgonzalezbrt@gmail.com>
2020-04-27 15:50:36 -0500

    [IMP] payroll number of days and hr_data TODO: number of days 30 - leve

--- l10n_ec_hr_payroll/models/hr_payroll.py
+++ l10n_ec_hr_payroll/models/hr_payroll.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import time
-from datetime import datetime, timedelta
+from datetime import datetime
+from datetime import time as datetime_time
 
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
@@ -29,6 +30,57 @@ class HrPayslip(models.Model):
         return False
 
     journal_id = fields.Many2one(default=_get_default_journal, )
+
+    @api.model
+    def get_worked_day_lines(self, contracts, date_from, date_to):
+        """
+        @param contract: Browse record of contracts
+        @return: returns a list of dict containing the input that should be applied for the given contract between date_from and date_to
+        """
+        res = []
+        # fill only if the contract as a working schedule linked
+        for contract in contracts.filtered(lambda contract: contract.resource_calendar_id):
+            day_from = datetime.combine(fields.Date.from_string(date_from), datetime_time.min)
+            day_to = datetime.combine(fields.Date.from_string(date_to), datetime_time.max)
+
+            # compute leave days
+            leaves = {}
+            day_leave_intervals = contract.employee_id.iter_leaves(day_from, day_to, calendar=contract.resource_calendar_id)
+            for day_intervals in day_leave_intervals:
+                for interval in day_intervals:
+                    holiday = interval[2]['leaves'].holiday_id
+                    current_leave_struct = leaves.setdefault(holiday.holiday_status_id, {
+                        'name': holiday.holiday_status_id.name or _('Global Leaves'),
+                        'sequence': 5,
+                        'code': holiday.holiday_status_id.name or 'GLOBAL',
+                        'number_of_days': 0.0,
+                        'number_of_hours': 0.0,
+                        'contract_id': contract.id,
+                    })
+                    leave_time = (interval[1] - interval[0]).seconds / 3600
+                    current_leave_struct['number_of_hours'] += leave_time
+                    work_hours = contract.employee_id.get_day_work_hours_count(interval[0].date(), calendar=contract.resource_calendar_id)
+                    if work_hours:
+                        current_leave_struct['number_of_days'] += leave_time / work_hours
+
+            # compute worked days
+            work_data = contract.employee_id.with_context(no_tz_convert=True).get_work_days_data(day_from, day_to, calendar=contract.resource_calendar_id)
+            
+            if(day_from.day == 1 and (day_to.day >= 30 or (day_to.day >= 28 and day_to.month == 2))):
+                work_data['days'] = 30
+
+            attendances = {
+                'name': _("Normal Working Days paid at 100%"),
+                'sequence': 1,
+                'code': 'WORK100',
+                'number_of_days': work_data['days'],
+                'number_of_hours': work_data['hours'],
+                'contract_id': contract.id,
+            }
+
+            res.append(attendances)
+            res.extend(leaves.values())
+        return res
 
     # def get_worked_day_lines(self, cr, uid, contract_ids, date_from, date_to, context=None):
     #     """
